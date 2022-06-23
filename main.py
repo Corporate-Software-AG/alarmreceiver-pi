f1ceaa3e1c382ee1e3c5c5edac3092882761aef0
Thomas <65050898+tvonment@users.noreply.github.com>
2022-06-23 14:00:13 +0000

    rewrite loop, add services

--- main.py
+++ main.py
@@ -22,16 +22,20 @@ GPIO.setup(Relay_Ch1, GPIO.OUT)
 GPIO.setup(Relay_Ch2, GPIO.OUT)
 GPIO.setup(Relay_Ch3, GPIO.OUT)
 
-async def main():
-    # The connection string for a device should never be stored in code. For the sake of simplicity we're using an environment variable here.
-    conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
 
+async def main():
+    print("Start...")
+    # The connection string for a device should never be stored in code. For the sake of simplicity we're using an environment variable here.
+    #conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
+    conn_str = 'HostName=bkd-alarmbot-test.azure-devices.net;DeviceId=DemoPi;SharedAccessKey=iReCiNIg/Ud+uFo2frg90JrErMb2GrF422pnIFuQK3M='
+
+    print("Set Default Relais Setup...")
     # Set default value for Relais 1
     GPIO.output(Relay_Ch1, GPIO.HIGH)
 
+    print("Connect to IoT Hub...")
     # The client object is used to interact with your Azure IoT hub.
     device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
-
     # connect the client.
     await device_client.connect()
 
@@ -65,10 +69,8 @@ async def main():
     # Define behavior for halting the application
     def stdin_listener():
         while True:
-            selection = input("Press Q to quit\n")
-            if selection == "Q" or selection == "q":
-                print("Quitting...")
-                break
+            print("Nothing Received\n")
+            time.sleep(5)
 
     # Run the stdin listener in the event loop
     loop = asyncio.get_running_loop()
@@ -83,8 +85,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-    # If using Python 3.6 use the following code instead of asyncio.run(main()):
-    # loop = asyncio.get_event_loop()
-    # loop.run_until_complete(main())
-    # loop.close()
